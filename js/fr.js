914d5fcaba45b955d8946494d64264256751f617
Anna <alinavyazovtseva@gmail.com>
2022-09-04 21:27:27 +0300

    first commit

---
+++ js/fr.js
@@ -0,0 +1,6 @@
+//alert("hello");
+const result = confirm("Are you here?");
+console.log (result);
+
+const answer = prompt ("do you have 18 years old","");
+console.log (typeof(answer));
---
+++ js/script.js
@@ -0,0 +1,32 @@
+let number = 5;
+const leftBorderwidth = 1;
+
+number =10;
+console.log(number);
+
+const obj = {
+  name: "Jhon",
+  age: 25,
+  isMarried: false
+};
+
+  console.log (obj.name);
+
+  let arr = ['plum', 'orange',];
+  console.log (arr[0]);
+   
+  const answer = prompt ("do you have 18 years old","");
+  console.log (typeof(answer));
+
+  const result = confirm("Are you here?");
+console.log (result);
+const answers = [];
+
+answers[0] = prompt ('whats your name ?','');
+answers[1] = prompt ('your last name?','');
+answers[2] = prompt ('How old are you?','');
+
+document.write (answers);
+
+
+
