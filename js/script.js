26e401dec02b73a7860343a89940337d0d3f340e
Alina <alinavyazovtseva@gmail.com>
2022-09-06 21:56:28 +0300

    project courses

--- js/script.js
+++ js/script.js
@@ -1,27 +1,48 @@
-let number = 5;
-const leftBorderwidth = 1;
+// let number = 5;
+// const leftBorderwidth = 1;
 
-number =10;
-console.log(number);
+// number =10;
+// console.log(number);
 
-const obj = {
-  name: "Jhon",
-  age: 25,
-  isMarried: false
+// const obj = {
+//   name: "Jhon",
+//   age: 25,
+//   isMarried: false
+// };
+
+//   console.log (obj.name);
+
+//   let arr = ['plum', 'orange',];
+//   console.log (arr[0]);
+   
+//   const answer = prompt ("do you have 18 years old","");
+//   console.log (typeof(answer));
+
+//   const result = confirm("Are you here?");
+// console.log (result);
+// const answers = [];
+
+// answers[0] = prompt ('whats your name ?','');
+// answers[1] = prompt ('your last name?','');
+// answers[2] = prompt ('How old are you?','');
+
+
+const number0fFilms = +prompt  ("how movies have already you watced?",'');
+const personalmovieDB =  {
+count: number0fFilms,
+movies: {},
+actors:{},
+genres: [],
+privat: false
 };
 
-  console.log (obj.name);
+console.log (personalmovieDB);
 
-  let arr = ['plum', 'orange',];
-  console.log (arr[0]);
-   
-  const answer = prompt ("do you have 18 years old","");
-  console.log (typeof(answer));
+const a = prompt ('one of the previously movies wachted?',''),
+      b = prompt ('Whats mark is movies?',''),
+      c = prompt ('one of the previously movies wachted?',''),
+      d = prompt ('Whats mark is movies?','');
 
-  const result = confirm("Are you here?");
-console.log (result);
-const answers = [];
-
-answers[0] = prompt ('whats your name ?','');
-answers[1] = prompt ('your last name?','');
-answers[2] = prompt ('How old are you?','');
+personalmovieDB.movies[a]=b;
+personalmovieDB.movies[c]=d;
+console.log(personalmovieDB);
