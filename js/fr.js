a887e557b51304b40064c87e443bc32155eac402
Alina <alinavyazovtseva@gmail.com>
2022-09-07 23:35:51 +0300

    add new

--- js/fr.js
+++ js/fr.js
@@ -1,6 +1,20 @@
-//alert("hello");
-const result = confirm("Are you here?");
-console.log (result);
-
-const answer = prompt ("do you have 18 years old","");
-console.log (typeof(answer));
+"use strict";
+// if (4==4){
+//   console.log ('ok!');
+// }
+const num = 50;
+switch (num) {
+    case 49:
+      console.log('неверно');
+      break;
+    case 100:
+      console.log('неверно');
+      break;
+    case 50:
+      console.log('в точку');
+      break;  
+    default:
+      console.log('done!') ;
+      break;   
+}
+  
